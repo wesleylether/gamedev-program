e9c071df883976e7d3921043a04e0e948c6c55e9
Wesley Lether <w.lether@lethermade.nl>
2026-07-21 11:52:00 +0200

    feat: Finished Module 2-1.2

---
+++ Source/UnrealProgramGame/TransformsAndMovement/DirectionalMovement.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Base/BaseMovementActor.h"
+#include "Components/ArrowComponent.h"
+#include "CoreMinimal.h"
+
+#include "DirectionalMovement.generated.h"
+
+class UArrowComponent;
+
+UCLASS()
+class UNREALPROGRAMGAME_API ADirectionalMovement : public ABaseMovementActor
+{
+	GENERATED_BODY()
+
+public:
+	ADirectionalMovement();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UArrowComponent* ArrowComponent;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
